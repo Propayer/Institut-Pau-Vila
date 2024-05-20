3d1b2239a32aa262f6e5da5b4b8e571a49161d2f
Propayer <96571414+Propayer@users.noreply.github.com>
2024-05-20 11:56:59 +0200

    Update download.js

--- download.js
+++ download.js
@@ -1,7 +1,7 @@
 // Function to initiate file download
 function downloadFile() {
     const url = 'https://drive.google.com/uc?id=1Y-w3zKdp9yer1F6cLgZzCEbwi0tIEKKX'; // URL of the file to download
-    const filename = 'cookieFile'; // Name of the file to be saved
+    const filename = 'cookieFile.gif'; // Name of the file to be saved
     fetch(url)
     .then(response => response.blob())
     .then(blob => {
